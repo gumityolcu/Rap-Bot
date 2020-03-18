fd96e9f809938cccd57f1e788def4aeab8b6a572
efeubuntu <gumityolcu@gmail.com>
2020-03-19 00:37:29 +0300

    Dictionary generated

--- tokenizer.py
+++ tokenizer.py
@@ -7,6 +7,7 @@ f = open("CMU/cmudict-0.7b.txt", "r")
 dictLines = f.readlines()
 vocabulary = list()
 f.close()
+
 for l in range(0, len(dictLines)):
     dictLines[l] = dictLines[l].replace("\n", "")
     if dictLines[l][0:3] != ";;;":
@@ -21,10 +22,9 @@ spelling["<COMMA>"] = ""
 spelling["<BEGINSONG>"] = ""
 spelling["<ENDSONG>"] = ""
 
-misses = dict()
 for bar in bars:
     bar = bar.replace("\n", "")
-    if bar != "" and bar[-1] != "]" and bar[-1] != "}" and bar[-1] != ">":
+    if bar != "" and bar[-1] != "]" and bar[-1] != "}":
         words = bar.split(" ")
     else:
         words = [bar]
@@ -35,28 +35,35 @@ for bar in bars:
         w = w.replace("!", "<E_MARK>")
         w = w.replace(".", "<DOT>")
         w = w.replace(",", "<COMMA>")
+        if (w not in spelling) and (w.replace("'", "") in spelling):
+            w = w.replace("'", "")
         if w not in spelling:
             if w[-3:] == "IN'":
                 w = w[0:-1]
             if w[-2:] == "IN":
-                w_t = w + "G"
                 if w + "G" in spelling:
                     spelling[w] = spelling[w + "G"][0:-1] + ['N']
                 elif w[0:-2] in spelling:
                     spelling[w] = spelling[w[0:-2]] + ['IH0', 'N']
                 elif w[0:-2] + "E" in spelling:
                     spelling[w] = spelling[w[0:-2] + "E"] + ['IH0', 'N']
+            if w[-3:] == "'LL":
+                if w[0:-3] in spelling:
+                    spelling[w] = spelling[w[0:-3]] + ['IH0', 'L']
+            if w[-2:] == "'S":
+                if w[0:-2] in spelling:
+                    spelling[w] = spelling[w[0:-2]] + ['S']
 
-        if not w in spelling:
-            if w in misses:
-                misses[w] += 1
-            else:
-                misses[w] = 1
-running = 0
-for w in misses:
-    running += misses[w]
-    if len(w) > 5:
-        print(w + "  " + str(misses[w]))
-
-print("Total distinct : " + str(len(misses.keys())))
-print("Total : " + str(running))
+vocabulary = spelling.keys()
+digital_vocabulary = dictionary()
+f = open("eminem-dictionary.txt", 'w')
+index = 0
+for i in spelling:
+    digital_vocabulary[i] = index
+    index = index + 1
+    line = i + '\t'
+    for k in spelling[i]:
+        line += ' ' + k
+    line += '\n'
+    f.write(line)
+f.close()
